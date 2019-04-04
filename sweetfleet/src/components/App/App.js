1e33231c2c40dfe56fb1b3359d27a2c60c1643b9
Joshua Vance <skinsfandc0612@icloud.com>
2019-04-04 17:06:12 -0400

    homepage needs some work but it is mostly complete

--- sweetfleet/src/components/App/App.js
+++ sweetfleet/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import '../Header/Header.css';
+import '../Homepage/Homepage.css';
 import Homepage from '../Homepage/Homepage';
 import Header from '../Header/Header';
 
@@ -8,8 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className='Home'>
-        <Homepage />
         <Header />
+        <Homepage />
       </div>
     );
   }
