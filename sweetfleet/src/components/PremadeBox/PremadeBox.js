697351c5ef526db5419bd63437ca69a807cb499c
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-04-04 13:56:12 -0400

    Shop and premade waiting for API

--- sweetfleet/src/components/PremadeBox/PremadeBox.js
+++ sweetfleet/src/components/PremadeBox/PremadeBox.js
@@ -2,7 +2,32 @@ import React, { Component } from 'react';
 
 class PremadeBox extends Component {
 	render() {
-		return <div />;
+		// let images = []
+		//for all of the images associated with this premade box, put em in the array
+		return (
+			<div>
+				{/* Display images as all the individual images in a lower row, and put the first image in the array autohighlighted in the upper area <img></img> */}
+				{/* <span>Premadebox Name</span> */}
+				{/* <span>Premadebox Price</span> */}
+				{/* <span>PremadeBox Description</span> */}
+				<span> Add a personal message:</span>
+				<input type="text"> </input>
+				<span> Quantity: </span>
+				<select>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+					<option value="10">10</option>
+				</select>
+				Big memes Bottom Text
+			</div>
+		);
 	}
 }
 
--- sweetfleet/src/components/Shop/Shop.js
+++ sweetfleet/src/components/Shop/Shop.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import './Shop.css';
+//import PremadeBox from '../PremadeBox/PremadeBox'
 
 class Shop extends Component {
 	render() {
-		return <div />;
+		//let PremadeBoxes = []
+		//For (each Premade Box entry in some premade box data set) {
+		//Grab the image, the name, and the description and put it into premade boxes
+		//}
+		return (
+			<div>
+				<span>CHOOSE A PREMADE BOX</span>
+				{/* PremadeBoxes */}
+				<span>General Description of premade boxes goes here, underneath all of the premade boxes</span>
+			</div>
+		);
 	}
 }
 
