4190fee511c289ec8e8554d78f4a00f7c3e2fefb
Joshua Vance <skinsfandc0612@icloud.com>
2019-04-04 14:19:17 -0400

    built out the header jfv

--- sweetfleet/src/components/App/App.js
+++ sweetfleet/src/components/App/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import '../Header/Header.css';
+import Homepage from '../Homepage/Homepage';
+import Header from '../Header/Header';
 
 class App extends Component {
-	render() {
-		return <div />;
-	}
+  render() {
+    return (
+      <div className='Home'>
+        <Homepage />
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
