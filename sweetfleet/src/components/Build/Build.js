0708af404f3f1b661aa6ebf8338c8bf274fad25b
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-04-05 12:02:31 -0400

    Pausing to merge

--- sweetfleet/src/components/Build/Build.js
+++ sweetfleet/src/components/Build/Build.js
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
 import Box_Style from "./Box_Style";
+import Sidebar from "../Sidebar/Sidebar";
 
 class Build extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			cost: 25,
+			design_name: "",
+			design_image: "",
+			title: ""
+		};
+		this.handleUpdate = this.handleUpdate.bind(this);
+	}
+
+	handleUpdate(event) {
+		this.setState({ [event.target.name]: event.target.value });
+		console.log(event.name);
+	}
 	render() {
 		return (
 			<div className="build">
 				<h2>
 					<em>Style Your Box</em>
 				</h2>
+				<Sidebar design_name={this.state.design_name} cost={this.state.cost} />
 				<div className="box_design">
 					<h3>CHOOSE BOX DESIGN</h3>
 					<div>
 						<Box_Style
 							image={"https://i.imgur.com/zhlanMv.jpgx"}
 							design="Cozy"
+							onClick={this.handleUpdate}
+							name="Cozy"
 						/>
+
 						<Box_Style
 							image={"https://i.imgur.com/w8TO2zig.jpg"}
 							design="Congrats"
@@ -31,9 +51,7 @@ class Build extends Component {
 						TITLE YOUR BOX <i class="fas fa-info-circle" />
 					</h3>
 					<div>
-						<div className="title_form">
-							<textarea placeholder="Type Box Name Here" />
-						</div>
+						<textarea placeholder="Type Box Name Here" />
 					</div>
 				</div>
 			</div>
