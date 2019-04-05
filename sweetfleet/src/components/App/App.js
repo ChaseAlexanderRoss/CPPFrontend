5258992167f165b40a3cc3e5fe56d0b0a6366ef3
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-04-05 10:39:40 -0400

    updated directory build

--- sweetfleet/src/components/App/App.js
+++ sweetfleet/src/components/App/App.js
@@ -5,8 +5,6 @@ import Footer from '../Footer/Footer';
 import Homepage from '../Homepage/Homepage';
 import { Link, Route, Switch } from 'react-router-dom';
 
-// import '../Homepage/Homepage.css';
-// import Homepage from '../Homepage/Homepage';
 import Header from '../Header/Header';
 
 class App extends Component {
