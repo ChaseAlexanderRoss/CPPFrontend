3ba50e34c71d7c3d502445a2c0df0a3e2b94b1ed
Neftali Valdez Calderon <neftalivaldez1@gmail.com>
2019-04-05 09:15:50 -0400

    updated react

--- sweetfleet/src/components/Footer/Footer.js
+++ sweetfleet/src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import "./Footer.css";
+import React, { Component } from 'react';
+import './Footer.css';
 
 class Footer extends Component {
   render() {
     return (
       <div>
         <section>
-          <div class="footerContainer">
-            <div class="aboutfleet">
+          <div class='footerContainer'>
+            <div class='aboutfleet'>
               <h4>About Sweet Fleet</h4>
               <p>
                 Sweet fleet provides beautiful, customized packages <br />
@@ -16,33 +16,31 @@ class Footer extends Component {
                 together across distances.
               </p>
             </div>
-            <div class="connect">
+            <div class='connect'>
               <h4>Connect</h4>
-              <ul style="list-style: none; ">
+              <ul>
                 <li>
                   <a
-                    target="_blank"
-                    title="follow me on instagram"
-                    href="https://www.instagram.com/PLACEHOLDER"
-                  >
+                    target='_blank'
+                    title='follow me on instagram'
+                    href='https://www.instagram.com/PLACEHOLDER'>
                     <img
-                      alt="follow me on instagram"
-                      src="https://c866088.ssl.cf3.rackcdn.com/assets/instagram30x30.png"
-                      border="0"
+                      alt='follow me on instagram'
+                      src='https://c866088.ssl.cf3.rackcdn.com/assets/instagram30x30.png'
+                      border='0'
                     />
-                    <span style="color:white">Instagram</span>
+                    {/* <span style="color:white">Instagram</span> */}
                   </a>
                 </li>
                 <li>
                   <a
-                    target="_blank"
-                    title="follow me on pinterest"
-                    href="https://www.pinterest.com/PLACEHOLDER"
-                  >
+                    target='_blank'
+                    title='follow me on pinterest'
+                    href='https://www.pinterest.com/PLACEHOLDER'>
                     <img
-                      alt="follow me on pinterest"
-                      src="https://c866088.ssl.cf3.rackcdn.com/assets/pinterest30x30.png"
-                      border="0"
+                      alt='follow me on pinterest'
+                      src='https://c866088.ssl.cf3.rackcdn.com/assets/pinterest30x30.png'
+                      border='0'
                     />
                   </a>
                   Pintrest
@@ -51,7 +49,7 @@ class Footer extends Component {
             </div>
             <div>
               <h4>Stay in touch</h4>
-              <ul style="list-style: none; ">
+              <ul>
                 <li>Contact Us</li>
                 <li>Subscribe to mailing list</li>
               </ul>
--- sweetfleet/src/components/Homepage/Homepage.js
+++ sweetfleet/src/components/Homepage/Homepage.js
@@ -1,70 +1,69 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardLink, CardText, CardBody } from 'reactstrap';
-import Birthday from '../../Images/Birthday.jpg';
-import Cozy from '../../Images/Cozy.jpg';
-import Congrats from '../../Images/Congrats.jpg';
-import Flower from '../../Images/Flower.png';
+import {
+  Card,
+  CardImg,
+  CardLink,
+  CardText,
+  CardImgOverlay,
+  Button,
+} from 'reactstrap';
+import Video from '../../Images/video.png';
+import './Homepage.css';
+import Shop from '../Shop/Shop';
 
 class Homepage extends Component {
   render() {
     return (
-      <div className='container'>
-        <Card>
-          <CardImg className='birthday' src={Birthday} alt='Card image cap' />
-          <CardBody>
-            <CardText>
-              Beautiful, customized packages full of cozy, memorable presents
-            </CardText>
-            <CardLink className='link' href='./shop'>
-              SHOP NOW
-            </CardLink>
-            <br />
-            <CardLink href='/build'>BUILD YOUR OWN</CardLink>
-          </CardBody>
+      <div>
+        <Card inverse>
+          <CardImg
+            width='100%'
+            src='https://d2i72ju5buk5xz.cloudfront.net/gsc/2TLY0R/70/a9/c5/70a9c556b10540a5bab2cd3936f3ea8b/images/home/u486.png?token=607cbcf2ce8be7abcbb4dc5692d9adc1'
+            alt='Card image cap'
+          />
+          <CardImgOverlay>
+            <div className='btns'>
+              <CardText className='text-muted'>
+                <h3>
+                  Beautiful, customized packages <br /> full of cozy, memorable
+                  presents
+                </h3>
+              </CardText>
+
+              <div className='buttons'>
+                <a className='link-shop' href='./shop'>
+                  SHOP NOW
+                </a>
+              </div>
+              <br />
+              <div className='buttons'>
+                <a className='link-build' href='./shop'>
+                  OR BUILD YOUR OWN
+                </a>
+              </div>
+              <br />
+            </div>
+          </CardImgOverlay>
         </Card>
-        <div>
-          <Card className='box'>
-            <CardBody>
-              <CardImg className='premade' src={Cozy} alt='Card image cap' />
-            </CardBody>
-            <CardBody>
-              <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </CardText>
-              <CardLink href='#'>Card Link</CardLink>
-            </CardBody>
-          </Card>
+        {/* shop component */}
+        <div className='shop'>
+          <Shop />
         </div>
-        <div>
-          <Card className='box'>
-            <CardBody>
-              <CardImg className='premade' src={Congrats} alt='Congrats' />
-            </CardBody>
-            <CardBody>
-              <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </CardText>
-              <CardLink href='#'>Card Link</CardLink>
-              <CardLink href='#'>Another Link</CardLink>
-            </CardBody>
-          </Card>
-        </div>
-        <div>
-          <Card className='box'>
-            <CardBody>
-              <CardImg className='flower' src={Flower} alt='Flower' />
-            </CardBody>
-            <CardBody>
-              <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </CardText>
-              <CardLink href='#'>Card Link</CardLink>
-              <CardLink href='#'>Another Link</CardLink>
-            </CardBody>
-          </Card>
+
+        <Card inverse>
+          <CardImg
+            width='100%'
+            src='https://d2i72ju5buk5xz.cloudfront.net/gsc/2TLY0R/70/a9/c5/70a9c556b10540a5bab2cd3936f3ea8b/images/home/u578.png?token=5b8020d1e99722e2bb7c299c04c274d7'
+            alt='Card image cap'
+          />
+          <CardImgOverlay>
+            <div className='background'>
+              <h3 className='background-header'>HOW IT WORKS</h3>
+            </div>
+          </CardImgOverlay>
+        </Card>
+        <div className='video'>
+          <img className='video-img' src={Video} />
         </div>
       </div>
     );
