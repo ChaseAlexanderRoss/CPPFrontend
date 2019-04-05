0e2fc8de98643cd4e562847f3fdadd8b1847f1f0
ChaseAlexanderRoss <chase.alexander.ross@gmail.com>
2019-04-05 11:42:32 -0400

    I'll be back in a sec

--- sweetfleet/src/components/Cart/Cart.js
+++ sweetfleet/src/components/Cart/Cart.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import './Cart.css';
 
 class Cart extends Component {
 	render() {
-		return <div />;
+		return (
+			<div>
+				<span>REVIEW YOUR CARTS</span>
+				<span>Cara's Breakup Survival Kit</span>
+				<img src="https://d2i72ju5buk5xz.cloudfront.net/gsc/2TLY0R/70/a9/c5/70a9c556b10540a5bab2cd3936f3ea8b/images/cart/u1752.png?token=d663ff51e9a0952fa31f2ad9b64b183b" />
+				<span>SHARE LINK</span>
+				{/* <div>copy link area, unsure how to do that</div> */}
+				<span>Close Date: {/*The date value*/} */</span>
+				<span>PRICE</span>
+				<span>QUANTITY</span>
+				<span>TOTAL</span>
+				<span>CUSTOM BOX</span>
+				<span>Contains</span>
+			</div>
+		);
 	}
 }
 
